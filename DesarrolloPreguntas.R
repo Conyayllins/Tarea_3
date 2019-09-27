36f2980dcc4f1c8987a43ab12f38cc24188c8dfc
conyayllins <conyayllins@gmail.com>
2019-09-27 19:29:08 -0300

    Avance hasta el ejercicio 5

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -1,8 +1,35 @@
-listaDeNumeros <- list(2,5,6,2,1, 5, 6, 1, 0, 1, 1, 2, 0, 15)
+listaDeNumeros <- list(2, 5, 6, 2, 1, 5, 6, 10, 11, 20, 15)
 listaDeNumeros[5]
 
-Ejercicio I
-
-Al reemplazar un 0 
+#Ejercicio 1: 
+# - Al reemplazar el 5 por el 0, me da como resultado list()
+# - Al reemplazar el 5 por un 15, me da como resultado [[1]] NULL, es decir que no existe.
+# - Al reemplazar el 5 por un -1, me da como resultado que quita de la posición número uno, el número que corresponde en la lista. el número 2 es el primero y el que no aparece. 
 
 listaDeNumeros [0]
+listaDeNumeros [15]
+listaDeNumeros [-1]
+
+#Ejercicio 2:
+# - Al ocupar el unlist la diferencia es de que este arroja sólo [1] 1
+# - Al ocupar el if en la función
+ 
+unlist(listaDeNumeros[5]) 
+if(5="tarea3"){
+  unlist(listaDeNumeros[5])
+}
+
+#Ejercicio 3:
+# al ejecutar listaDeNumeros[5] <- 12 me da el mismo resultado en la consola.  
+
+listaDeNumeros[5] <- 12
+
+#Ejercicio 4:
+# - el nombre_variable debe ser reemplazado por listaDeNumeros.
+# - al ejecutar length me arroja el total de variables que contiene la lista. 
+
+length(listaDeNumeros)
+
+#Ejercicio 5:
+
+
